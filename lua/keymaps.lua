6722470e7a1e151584ec1b7af09fc9409ae33012
ghos7s <freddyfrolov383@gmail.com>
2025-08-22 17:33:09 +0200

    Commit N4. Refactored lua/keymaps.lua module.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader = '<Space>'
+vim.g.mapleader = '<space>'
 vim.keymap.set('n', '<leader>ga', ':Git add .<CR>', {})
 vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', {})
 vim.keymap.set('n', '<leader>gp', ':Git push<CR>', {})
